5ab629f8f5e0f2e71e5ee395d310ef112c03da09
Francisco Santos <3703930+thaenor@users.noreply.github.com>
2026-01-21 12:21:14 +0000

    Add PWA support and various image assets for AvoDoku
    - Integrated VitePWA plugin for Progressive Web App capabilities.
    - Configured PWA manifest with app details including name, description, and icons.
    - Added runtime caching for Google Fonts to improve performance.
    - Included multiple image assets for both iOS and Windows 11 platforms.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,74 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'AvoDoku',
+        short_name: 'AvoDoku',
+        description: 'Sudoku for everyone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'landscape',
+        scope: '/AvoDoku/',
+        start_url: '/AvoDoku/',
+        icons: [
+          {
+            src: 'android/android-launchericon-192-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android/android-launchericon-512-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+       workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   base: '/AvoDoku/',
   server: {
     open: false,
